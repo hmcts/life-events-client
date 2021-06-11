d686ef785d4c667495ae4854a8a05f8914d7b929
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2021-06-11 16:16:22 +0100

    Return null when record not found.

--- src/main/java/com/github/hmcts/lifeevents/client/service/DeathServiceImpl.java
+++ src/main/java/com/github/hmcts/lifeevents/client/service/DeathServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.github.hmcts.lifeevents.client.api.DeathApiClient;
 import com.github.hmcts.lifeevents.client.model.V1Death;
+import feign.FeignException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -34,6 +35,9 @@ public class DeathServiceImpl implements DeathService {
     public V1Death getDeathRecordById(Integer id) {
         try {
             return deathApiClient.readV1Death(id).getBody();
+        } catch (FeignException.FeignClientException.NotFound e) {
+            logger.info("LEV record not found:" + id);
+            return null;
         } catch (Exception e) {
             logger.error("Error during LEV call", e);
             throw e;
--- src/test/java/com/github/hmcts/lifeevents/client/service/DeathServiceTest.java
+++ src/test/java/com/github/hmcts/lifeevents/client/service/DeathServiceTest.java
@@ -1,11 +1,11 @@
 package com.github.hmcts.lifeevents.client.service;
 
-import com.github.hmcts.lifeevents.client.api.DeathApiClient;
-import com.github.hmcts.lifeevents.client.model.V1Death;
-
 import java.time.LocalDate;
 import java.util.List;
 
+import com.github.hmcts.lifeevents.client.api.DeathApiClient;
+import com.github.hmcts.lifeevents.client.model.V1Death;
+import feign.FeignException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +15,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -30,10 +32,18 @@ public class DeathServiceTest {
 
     @MockBean
     private ResponseEntity<List<V1Death>> results;
+    
 
+    
     @MockBean
     private List<V1Death> list;
-
+    
+    @MockBean
+    private ResponseEntity<V1Death> result;
+    
+    @MockBean
+    private V1Death record;
+    
     @Test
     void test(){
         when(deathApiClient.searchV1Death(any(),any(),any())).thenReturn(results);
@@ -41,5 +51,28 @@ public class DeathServiceTest {
 
         final List<V1Death> v1Deaths = deathService.searchForDeathRecordsByNamesAndDate("", "", LocalDate.now());
         assertThat(v1Deaths).isEqualTo(list);
+    }
+    
+    @Test
+    void shouldReturnDeathRecord(){
+        
+        when(deathApiClient.readV1Death(any())).thenReturn(result);
+        when(result.getBody()).thenReturn(record);
+
+        V1Death v1Death = deathService.getDeathRecordById(123456);
+        assertThat(v1Death).isEqualTo(record);
+    }
+
+    @Test
+    void shouldReturnNullWhenDeathRecordNotFound(){
+        when(deathApiClient.readV1Death(any())).thenThrow(FeignException.FeignClientException.NotFound.class);
+        V1Death v1Death = deathService.getDeathRecordById(123456);
+        assertNull(v1Death);
+    }
+
+    @Test
+    void testException(){
+        when(deathApiClient.searchV1Death(any(),any(),any())).thenThrow(new RuntimeException("Exception test"));
+        assertThrows(RuntimeException.class, () -> deathService.searchForDeathRecordsByNamesAndDate("", "", LocalDate.now()));
     }
 }
