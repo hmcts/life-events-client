7e7c21b51f397e210cec2441a63e27adb0f02185
FeliTam <feli.tam@justice.gov.uk>
2023-07-17 17:17:14 +0100

    DTSPB-3654 SpringBoot 3 upgrade

--- src/main/java/com/github/hmcts/lifeevents/client/api/DeathApiClient.java
+++ src/main/java/com/github/hmcts/lifeevents/client/api/DeathApiClient.java
@@ -3,6 +3,6 @@ package com.github.hmcts.lifeevents.client.api;
 import com.github.hmcts.lifeevents.client.config.ClientConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name="${lev.death.name:death}", url="${spring.security.oauth2.client.provider.homeoffice.authorization-uri:https://api.lev.homeoffice.gov.uk}", configuration = ClientConfiguration.class)
+@FeignClient(name="death", url="${spring.security.oauth2.client.provider.homeoffice.authorization-uri:https://api.lev.homeoffice.gov.uk}", configuration = ClientConfiguration.class)
 public interface DeathApiClient extends DeathApi {
 }
