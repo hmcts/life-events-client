963800866a8b04edc9f297064ebbd08e3fe7cd57
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2026-02-16 19:53:38 +0000

    DTSPB-4795 adding integration-test.yml

--- src/integrationTest/java/com/github/hmcts/lifeevents/client/api/DeathApiClientTest.java
+++ src/integrationTest/java/com/github/hmcts/lifeevents/client/api/DeathApiClientTest.java
@@ -1,32 +1,21 @@
 package com.github.hmcts.lifeevents.client.api;
 
-import java.time.LocalDate;
-import java.util.List;
-
 import com.github.hmcts.lifeevents.client.config.ClientConfiguration;
 import com.github.hmcts.lifeevents.client.config.ServiceConfiguration;
 import com.github.hmcts.lifeevents.client.model.V1Death;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(classes = { ClientConfiguration.class, ServiceConfiguration.class }, properties = {
-    "lev.death.url=https://api.life-event-verification.np.homeoffice.gov.uk"
-})
+@SpringBootTest(classes = { ClientConfiguration.class, ServiceConfiguration.class })
 @ActiveProfiles("test")
-@EnableConfigurationProperties
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration()
-@EnableAutoConfiguration
 class DeathApiClientTest {
 
     @Autowired
